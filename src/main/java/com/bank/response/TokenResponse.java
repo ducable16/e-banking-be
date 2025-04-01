0a57274c5d51286cda864bd69101586949f7a071
ducable16 <ductrant1k29@gmail.com>
2025-04-01 19:05:35 +0700

    add security base

---
+++ src/main/java/com/bank/response/TokenResponse.java
@@ -0,0 +1,13 @@
+package com.bank.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class TokenResponse {
+    private String accessToken;
+    private String refreshToken;
+}
